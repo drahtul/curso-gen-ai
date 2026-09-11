7be9cd58764f083315921e2c9fba5c98884dbcd9
Danilo Fraque <danilofraque@gmail.com>
2026-09-10 18:49:28 -0600

    Comienzo unidad 8.

---
+++ Unidad 8 - MCP/demo1-weather-mcp-client.py
@@ -0,0 +1,17 @@
+import asyncio
+import json
+from fastmcp import Client
+
+async def main():
+    client = Client("demo1-weather-mcp-server.py")
+    async with client:
+        
+        result = await client.list_tools()
+        result_dicts = [tool.__dict__ for tool in result]
+        print(json.dumps(result_dicts, indent=2))
+
+        forecast = await client.call_tool("get_forecast", {"latitude": 40.7128, "longitude": -74.0060})
+        print("\nForecast Result:")
+        print(forecast.data.result)
+
+asyncio.run(main())
